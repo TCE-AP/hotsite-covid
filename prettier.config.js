ba6a1bb688e5520448e765da2e1e9f3556ac9d88
andrefigueira93 <andrefigueira93@gmail.com>
2020-08-04 12:44:59 -0300

    Ajuste nas Configurações básicas do Projeto

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-}
+  trailingComma: 'all',
+  allowParens: 'avoid',
+};
